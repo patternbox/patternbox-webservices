de5494f4e62ecf814adc311732f8f7a1deaec839
patternbox <ehms@patternbox.com>
2014-04-13 14:28:34 +0200

    Library domain model added

---
+++ src/main/java/com/patternbox/webservices/domain/model/Author.java
@@ -0,0 +1,83 @@
+/**************************** Copyright notice ********************************
+
+Copyright (C)2014 by D. Ehms, http://www.patternbox.com
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+ ******************************************************************************/
+package com.patternbox.webservices.domain.model;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ * @author <a href='http://www.patternbox.com'>D. Ehms, Patternbox</a>
+ */
+@Entity
+@NamedQueries({
+		@NamedQuery(name = "Author.findAll", query = "SELECT a FROM Author a ORDER BY a.lastName, a.firstName"),
+		@NamedQuery(name = "Author.findByName", query = "SELECT a FROM Author a WHERE a.lastName = :lastName") })
+public class Author {
+
+	@Id
+	private String email;
+
+	private String firstName;
+
+	private String lastName;
+
+	@ManyToMany
+	@JoinTable(name = "Author_X_Literature", joinColumns = { @JoinColumn(name = "author") }, inverseJoinColumns = { @JoinColumn(name = "isbn") })
+	private final Set<Literature> literatures = new TreeSet<Literature>();
+
+	/**
+	 * Default constructor to satisfy JPA
+	 */
+	public Author() {
+		super();
+	}
+
+	/**
+	 * Parameterized constructor
+	 */
+	public Author(String email, String firstName, String lastName) {
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Author [email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", literatures=" + literatures + "]";
+	}
+}
